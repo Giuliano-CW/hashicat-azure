62a4f0a6ed460c9932cd16489e39cd7eea654880
Giuliano Di Biase <giuliano.dibiase@controlware.de>
2022-07-25 11:00:39 +0000

    test

--- main.tf
+++ main.tf
@@ -111,6 +111,9 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
+  Billable = "true"
+  Department = "test"
+
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
